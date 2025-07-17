beaffb2e676b32e850f8029fa2c1d1120c5a98e3
조동현 <ehdgus309@naver.com>
2025-07-18 01:57:45 +0900

    swagger 작업

--- src/main/java/kr/hhplus/be/server/controller/coupon/CouponController.java
+++ src/main/java/kr/hhplus/be/server/controller/coupon/CouponController.java
@@ -1,39 +1,63 @@
 package kr.hhplus.be.server.controller.coupon;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.dto.ResponseDto;
 import kr.hhplus.be.server.dto.coupon.CouponIssueRequestDto;
 import kr.hhplus.be.server.mock.FakeData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/coupons")
+@Tag(name = "Coupon", description = "쿠폰 관련 API")
 public class CouponController {
-	
-	private FakeData fakeData;
+
+    private final FakeData fakeData;
 
     @Autowired
-    public CouponController(FakeData fakeData){
+    public CouponController(FakeData fakeData) {
         this.fakeData = fakeData;
     }
-	
-	@PostMapping("/issue")
-	public ResponseEntity<ResponseDto> issueCoupon(@RequestBody CouponIssueRequestDto requestDto ){
-		ResponseDto res = new ResponseDto();
-		try {
-			res.setData(fakeData.issueCoupon(requestDto));
-		}catch(Exception e) {
-			e.printStackTrace();
-			res.setCode(500);
-			res.setMessage("잔액충전에 실패하였습니다.");
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
-		}
-		return ResponseEntity.ok(res);
-	}
+
+    @PostMapping("/issue")
+    @Operation(
+        summary = "쿠폰 발급",
+        description = "유저 ID와 쿠폰 ID를 입력받아 쿠폰을 발급합니다.",
+        requestBody = @RequestBody(
+            description = "쿠폰 발급 요청",
+            required = true,
+            content = @Content(
+                schema = @Schema(implementation = CouponIssueRequestDto.class),
+                examples = @ExampleObject(
+                    value = "{\"userId\": 1, \"couponId\": 100}"
+                )
+            )
+        ),
+        responses = {
+            @ApiResponse(responseCode = "200", description = "쿠폰 발급 성공"),
+            @ApiResponse(responseCode = "500", description = "쿠폰 발급 실패")
+        }
+    )
+    public ResponseEntity<ResponseDto> issueCoupon(
+        @org.springframework.web.bind.annotation.RequestBody CouponIssueRequestDto requestDto) {
+
+        ResponseDto res = new ResponseDto();
+        try {
+            res.setData(fakeData.issueCoupon(requestDto));
+        } catch (Exception e) {
+            e.printStackTrace();
+            res.setCode(500);
+            res.setMessage("쿠폰발급에 실패하였습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(res);
+        }
+        return ResponseEntity.ok(res);
+    }
 }
