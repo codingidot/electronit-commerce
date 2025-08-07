42bcc2b8199fa32499e29b252b2a1ad2b178fcad
조동현 <ehdgus309@naver.com>
2025-08-08 02:37:01 +0900

    주문시 재고차감 낙관적락으로 처리

--- src/main/java/kr/hhplus/be/server/product/entity/ProductEntity.java
+++ src/main/java/kr/hhplus/be/server/product/entity/ProductEntity.java
@@ -2,6 +2,8 @@ package kr.hhplus.be.server.product.entity;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Version;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +20,9 @@ public class ProductEntity {
     private Integer stock;
     private String goodsType;
     private Long repreGoodsId;
+    
+    @Version
+    Long version;
     
     public ProductEntity(){};
 
@@ -45,8 +50,13 @@ public class ProductEntity {
 		return repreGoodsId;
 	};
 	
-	public void deductStock(int cnt) {
+	public void deductStock(int cnt) throws Exception {
+		if(this.stock < cnt) throw new Exception("재고가 부족합니다.");
 		this.stock -= cnt;
+	}
+
+	public Long getVersion() {
+		return version;
 	}
 
 	public void setGoodsId(Long goodsId) {
