ff1285225dfec59f7cd45a0532d7a77f63e9f189
조동현 <ehdgus309@naver.com>
2025-08-17 19:47:51 +0900

    OrderResponse, BalanceResponse 레코드로 변환

--- src/main/java/kr/hhplus/be/server/order/dto/OrderResponse.java
+++ src/main/java/kr/hhplus/be/server/order/dto/OrderResponse.java
@@ -4,19 +4,14 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import kr.hhplus.be.server.order.enums.OrderState;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class OrderResponse {
-
-	Long goodsId;
-	Long userId;
-	Long couponId;
-	int count;
-	BigDecimal orderPrice;
-	BigDecimal payPrice;
-	OrderState orderState;
-	LocalDateTime orderDate;
-}
+public record OrderResponse(
+	    Long goodsId,
+	    Long userId,
+	    Long couponId,
+	    int count,
+	    BigDecimal orderPrice,
+	    BigDecimal payPrice,
+	    OrderState orderState,
+	    LocalDateTime orderDate
+	) {}
--- src/main/java/kr/hhplus/be/server/user/dto/BalanceResponse.java
+++ src/main/java/kr/hhplus/be/server/user/dto/BalanceResponse.java
@@ -2,12 +2,5 @@ package kr.hhplus.be.server.user.dto;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class BalanceResponse {
-
-	private BigDecimal balance;
-}
+public record BalanceResponse(BigDecimal balance) {}
