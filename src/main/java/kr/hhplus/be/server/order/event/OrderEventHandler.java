1742fdddf5769085689568cca836b41ceba40c56
조동현 <ehdgus309@naver.com>
2025-08-30 14:21:12 +0900

    @EventListener 추가

--- src/main/java/kr/hhplus/be/server/order/event/OrderEventHandler.java
+++ src/main/java/kr/hhplus/be/server/order/event/OrderEventHandler.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.order.event;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 public class OrderEventHandler {
 
 	@Async
+	@EventListener
     public void handleOrderCreated(OrderCreatedEvent event) {
         log.info("✅ 주문 생성 이벤트 수신 - 주문ID: {}, 사용자ID: {}, 총액: {}",
                 event.getOrderId(), event.getUserId(), event.getTotalPrice());
