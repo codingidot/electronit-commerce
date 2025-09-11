5917e6ea48e67e07ae72d60aea1c23d317353d07
조동현 <ehdgus309@naver.com>
2025-09-11 22:47:48 +0900

    jdbc url 수정 및 쿠폰 발급 요청 dto 기본 생성자 추가

--- src/main/java/kr/hhplus/be/server/coupon/dto/CouponIssueRequest.java
+++ src/main/java/kr/hhplus/be/server/coupon/dto/CouponIssueRequest.java
@@ -2,9 +2,11 @@ package kr.hhplus.be.server.coupon.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CouponIssueRequest {
 	
 	private Long userId;
